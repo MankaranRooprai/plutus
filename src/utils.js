493c4bb1d2838bf334cbd42fd987f81810ca492e
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-09-17 18:15:12 -0400

    Finished app. Implemented all features.

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,5 @@
-//undefined locales means that the user's settings language is passed
 export const currencyFormatter = new Intl.NumberFormat(undefined, {
-    currency: 'cad',
-    style: 'currency',
-    minimumFractionDigits: 0
-})
+    currency: "usd",
+    style: "currency",
+    minimumFractionDigits: 0,
+  })
